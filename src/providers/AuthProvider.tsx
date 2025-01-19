63db610bcbaf6b53d25383d9e9ebf4bb19f75d52
mdaliakbor <aliakbor.js@gmail.com>
2025-01-19 17:22:23 +0600

    create and store jwt token in local storage

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -2,6 +2,7 @@ import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged,
 import { ReactNode, useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
 import auth from '@/firebase/firebase.init';
+import useAxiosPublic from '@/hooks/useAxiosPublic';
 
 
 type Props = {
@@ -13,6 +14,7 @@ const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}: Props) => {
   const [user, setUser] = useState<User>({} as User);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic()
 
   // create a new user
   const createUser = (email: string, password: string) => {
@@ -47,7 +49,18 @@ const AuthProvider = ({children}: Props) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user as User);
-      console.log("User from observer: ", user);
+      // console.log("User from observer: ", user);
+      if(user?.email){
+        axiosPublic.post('/jwt',{email: user?.email})
+        .then(res=>{
+          localStorage.setItem('access-token', res.data.token)
+          console.log(res.data.token)
+          setLoading(false);
+        })
+      }else{
+        localStorage.removeItem('access-token')
+      }
+      
     });
     return () => {
       unsubscribe();
--- src/routes/PrivetRoute.tsx
+++ src/routes/PrivetRoute.tsx
@@ -1,12 +1,12 @@
-
+import Spinner from "@/components/common/Spinner";
 import useAuth from "@/hooks/useAuth";
 import { Navigate } from "react-router";
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const isAuthenticated = Boolean(user?.email);
 
-
+  if (loading) return <Spinner />;
 
   return isAuthenticated ? children : <Navigate to='/login' />;
 };
