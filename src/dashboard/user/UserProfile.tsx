ce917d6ea8e28558d21d16b26a933e7e179607e2
mdaliakbor <aliakbor.js@gmail.com>
2025-01-22 00:58:41 +0600

    user profile data show and update profile all done

--- src/dashboard/user/UserProfile.tsx
+++ src/dashboard/user/UserProfile.tsx
@@ -1,6 +1,21 @@
+import Profile from "@/components/common/profile/Profile";
+import useAuth from "@/hooks/useAuth";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import { UserType } from "@/utils/userType";
+import { useQuery } from "@tanstack/react-query";
+
 
 const UserProfile = () => {
-    return <div>UserProfile UserProfile</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data, refetch } = useQuery<UserType>({
+    queryKey: ["user", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/${user?.email}`);
+      return res.data;
+    },
+  });
+  return <div>{data && <Profile data={data} refetch={refetch} />}</div>;
 };
 
 export default UserProfile;
